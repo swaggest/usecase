ca119b0cce05ba869b77cecfbbf844dc92e61a25
Viacheslav Poturaev <vearutop@gmail.com>
2021-10-03 00:13:36 +0200

    Update test

--- generic_test.go
+++ generic_test.go
@@ -12,6 +12,23 @@ import (
 	"github.com/swaggest/usecase"
 )
 
+func TestNewIOI_classic(t *testing.T) {
+	u := usecase.NewIOI(*new(int), new(string), func(ctx context.Context, input, output interface{}) error {
+		in := input.(int)
+		out := output.(*string)
+
+		*out = strconv.Itoa(in)
+
+		return nil
+	})
+
+	ctx := context.Background()
+
+	var out string
+	assert.NoError(t, u.Interact(ctx, 123, &out))
+	assert.Equal(t, "123", out)
+}
+
 func TestNewInteractor(t *testing.T) {
 	u := usecase.NewInteractor(func(ctx context.Context, input int, output *string) error {
 		*output = strconv.Itoa(input)
